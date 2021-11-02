c9d96639c691262bc0584730b85162568f30795a
Ashen <ashen.vm@gmail.com>
2021-11-02 23:53:27 +0100

    added env vars

--- listing-dev.sh
+++ listing-dev.sh
@@ -2,4 +2,4 @@
 docker pull tripzalk/tripza-listing-service:latest 
 docker stop listing-service 
 docker system prune -f 
-docker run -d --name=listing-service --network=tripza_dockr -e "SPRING_PROFILES_ACTIVE=dev" tripzalk/tripza-listing-service:latest
+docker run -d --name=listing-service --network=tripza_dockr --env-file ./.env tripzalk/tripza-listing-service:latest
