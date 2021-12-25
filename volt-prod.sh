6daeea2d9569d9acfc913ca4c2f85bf3038f5381
Ashen <ashen.vm@gmail.com>
2021-12-25 23:54:35 +0100

    Added file service

---
+++ volt-prod.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+docker pull tripzalk/tripza-file-service:latest 
+docker stop file-service 
+docker system prune -f 
+docker run -d --name=file-service --network=tripza_dockr -e "SPRING_PROFILES_ACTIVE=prod" tripzalk/tripza-file-service:latest
